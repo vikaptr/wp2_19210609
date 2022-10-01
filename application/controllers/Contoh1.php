fb95f99df5781393511d0ce1d5a0f12481e1426f
vikaptr <ayunovikaputri069c@gmail.com>
2022-10-01 13:39:56 +0700

    Menambahkan Index

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "
+		Nama saya Ayu Novika Putri <br>
+		Saya tinggal didaerah Tigaraksa <br>
+		olah raga yang saya sukai adalah
+		volly";
+	}
+}
+		
